fac4b04361643b938ed2630ff05954ad1e2f3277
Tanim-Ahmmed <tanimahmmed234@gmail.com>
2025-01-06 18:41:21 +0600

    formate done

--- src/pages/componants/About.jsx
+++ src/pages/componants/About.jsx
@@ -1,4 +1,4 @@
-import Pic from "../../assets/logo.png"
+import Pic from "../../assets/logo.png";
 
 const About = () => {
   return (
@@ -11,20 +11,28 @@ const About = () => {
           <h2 className="text-2xl lg:text-3xl font-bold mb-5 mt-5 dark:text-slate-300">
             About me
           </h2>
-          <h2 className="text-4xl lg:text-5xl font-bold mb-2 mt-5 dark:text-slate-300"> <span className="text-orange-400">Tanim</span> Ahmmed
+          <h2 className="text-4xl lg:text-5xl font-bold mb-2 mt-5 dark:text-slate-300">
+            {" "}
+            <span className="text-orange-400">Tanim</span> Ahmmed
           </h2>
           <h5 className=" font-semibold mb-5 dark:text-slate-300">
             Web Developer
           </h5>
-          
           <div className="flex flex-col items-center justify-center mb-12">
-          <p className="text-lg text-gray-700 max-w-xl mb-4">
-            Hello! I'm a passionate web developer with a focus on frontend technologies like React. I enjoy building beautiful, user-friendly interfaces and constantly learning new technologies to enhance my skills. I'm always looking for opportunities to contribute to impactful projects and collaborate with talented teams.📊
-          </p>
-          <p className="text-md text-gray-500 max-w-xl">
-            My goal is to create intuitive, responsive, and fast applications that make users' lives easier and more enjoyable. In my spare time, I like exploring open-source projects, learning new frameworks, and improving my problem-solving abilities.🚀
-          </p>
-        </div>
+            <p className="text-lg text-gray-700 max-w-xl mb-4">
+              Hello! I'm a passionate web developer with a focus on frontend
+              technologies like React. I enjoy building beautiful, user-friendly
+              interfaces and constantly learning new technologies to enhance my
+              skills. I'm always looking for opportunities to contribute to
+              impactful projects and collaborate with talented teams.📊
+            </p>
+            <p className="text-md text-gray-500 max-w-xl">
+              My goal is to create intuitive, responsive, and fast applications
+              that make users' lives easier and more enjoyable. In my spare
+              time, I like exploring open-source projects, learning new
+              frameworks, and improving my problem-solving abilities.🚀
+            </p>
+          </div>
         </div>
         <div className="md:w-2/5">
           <div className="overflow-hidden">
